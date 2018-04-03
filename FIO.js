dbc53c6a82498bb27b4fb612a5a5c347cbf98c4a
Coder-Channel <33266814+Coder-Channel@users.noreply.github.com>
2018-04-03 17:28:42 +0300

    Delete FIO.js

--- FIO.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (Creator, OnClickFunction) {
-    let FIOText = document.createElement("p");
-    FIOText.innerHTML = (Creator.SecondName + " " + Creator.Name + " " + Creator.Patronymic);
-    FIOText.onclick = OnClickFunction;
-    document.body.appendChild(FIOText);
-}
